a1773eaa38d96bbce399cd059edee9e5f020bf48
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-07-06 01:32:58 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0) {
+        let positiveNum = -n;
+        return +positiveNum.toString().split('').reverse().join('');
+      }
+      return +n.toString().split('').reverse().join('');
 }
